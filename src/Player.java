8381ce8f12b414d71f6679cb82da5f269bbe0df3
SMakela <73409799+smakela13@users.noreply.github.com>
2021-11-06 19:16:42 -0400

    Added comments to the Player class - Created tests to make sure valid conditions are met in constructor - Made Player class final since it's not currently inheriting

--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class Player {
+public final class Player {
    final Random rand = new Random();
 
    // Player variables
@@ -9,27 +9,40 @@ public class Player {
    private int healthPotions;
    private static final int HEALTH_POTION_HEAL_AMOUNT = 30;
    
+   // Player's default constructor
    public Player() {
       this(100, 1);
    }
 
+   // Player constructor that assigns value to health and health potions and tests if valid conditions are met
    public Player(int health, int healthPotions) {
+      if (health < 0) {
+         throw new IllegalArgumentException("Player health must be greater than 0");
+      }
       this.health = health;
+
+      if (healthPotions < 0) {
+         throw new IllegalArgumentException("Player health potions cannot be negative");
+      }
       this.healthPotions = healthPotions;
    }
 
+   // Player (potentially) takes damage from the enemy
    public void takeDamage(int damage) {
       this.health -= damage;
    }
 
+   // Checks if player is dead
    public boolean isDead() {
       return this.health <= 0;
    }
 
+   // Checks if player has health potions
    public boolean hasHealthPotions() {
       return this.healthPotions > 0;
    }
 
+   // Player uses health potion, if they have any
    public boolean useHealthPotion() {
       if (!hasHealthPotions()) {
          return false;
@@ -39,10 +52,12 @@ public class Player {
       return true;
    }
 
+   // Player potentially receives health potions from enemy's death
    public void receiveHealthPotion() {
       this.healthPotions++;
    }
 
+   // Player's getters and setters
    public int getHealth() {
       return health;
    }
